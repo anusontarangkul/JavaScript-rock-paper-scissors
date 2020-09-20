846990914b1bf6e49aff04db9f74c596e1f37ebe
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-20 16:09:13 -0700

    add win function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,13 +14,28 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+}
+
+function lose() {
+  console.log("Lost");
+}
+
+function tie() {
+  console.log("Tie");
+}
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
     case "rs":
     case "pr":
     case "sp":
-      win();
+      win(userChoice, computerChoice);
       break;
     case "rp":
     case "ps":
