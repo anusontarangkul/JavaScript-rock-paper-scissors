89f30839d2eed39cb1a14ee3d238deb9ac0a93b1
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-20 16:40:25 -0700

    add draw and lose function

--- app.js
+++ app.js
@@ -24,15 +24,27 @@ function win(userChoice, computerChoice) {
   userScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  result_div.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_div.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats
+    ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
 }
 
-function lose() {
-  console.log("Lost");
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_div.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to 
+    ${convertToWord(computerChoice)}${smallCompWord}. You lost..`;
 }
 
-function tie() {
-  console.log("Tie");
+function tie(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_div.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals
+    ${convertToWord(computerChoice)}${smallCompWord}. Its a draw!`;
 }
 
 function game(userChoice) {
